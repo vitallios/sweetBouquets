e21b94f3c44db17bb804396acf6611d4ae995d3c
Vitallios <vitallios.design@gmail.com>
2020-11-18 18:01:28 +0300

    галерея

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ document.getElementById('top').onclick = function () {
 	window.scrollTo(0, 0);
 }
 
+// function top() {
+// 	let btn = $('.top');
+
+// 	$(window).on('scroll', () => {
+// 		if ($(this).scrollTop() >= 50) {
+// 			btn.fadeIn();
+// 		} elis{
+// 			btn.fadeOut();
+// 		}
+// 	});
+
+// 	btn.on('click', (e) => {
+// 		e.preventDefault();
+// 		$('html').animate({ scrollTop: 0 }, 1000);
+// 	})
+// }
+
+// top();
